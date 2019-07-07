ce62cf82960333ba19fbd8bd68877d8d4840eaf2
Alexander <blacksonj7@gmail.com>
2019-07-07 20:49:40 +0200

    start

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'autodelete': {
+        'en': 'Delete Countdown when end is reached',
+        'de': 'Countdown löschen wenn Zeitpunkt erreicht wurde'
+    },
+    'inWordsShort': {
+        'en': 'In words (short)',
+        'de': 'In Worten (kurz)'
+    },
+    'inWordsLong': {
+        'en': 'In words (long)',
+        'de': 'In Worten (lang)'
+    },
+    'totalNoOfDays': {
+        'en': 'Total No. of Days',
+        'de': 'Gesamtanzahl Tagen'
+    },
+    'totalNoOfHours': {
+        'en': 'Total No. of Hours',
+        'de': 'Gesamtanzahl Stunden'
+    },
+    'totalNoOfWeeks': {
+        'en': 'Total No. of Weeks',
+        'de': 'Gesamtanzahl Wochen'
+    },
+    'endDate': {
+        'en': 'Enddate',
+        'de': 'Enddatum'
+    },
+    'tablecontent': {
+        'en': 'Show these fields in table:',
+        'de': 'Folgende Felder in Tabelle anzeigen:'
+    },
+    'dateFormat': {
+        'en': 'Date Format:',
+        'de': 'Datumsformat:'
+    }       
+};
