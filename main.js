50d418aeb26191f40e16d90d5d82baba6716e015
Alexander <alexander.kolb@outlook.at>
2021-06-20 21:46:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ function requestAtom(){
     adapter.log.info('Requesting data from ' + urlAtom)
     request.post({
         url:     urlAtom,
-       json: true,
+       //json: true,
         timeout: 8000
       }, function(error, response, body){
         if (error){
