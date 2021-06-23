f863e1d7276b867914c24ae229738e218a61383c
Alex <jack-blackson@users.noreply.github.com>
2021-06-23 17:21:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -241,8 +241,8 @@ async function processDetails(content, countInt){
     adapter.log.info(level)
     adapter.log.info(parameter)
     adapter.log.info(JSON.stringify(parameter))
-    adapter.log.info(JSON.stringify(parameter[0].awareness_level))
-    adapter.log.info(JSON.stringify(parameter[0].awareness_level.value))
+    adapter.log.info(JSON.stringify(parameter.awareness_level))
+    adapter.log.info(JSON.stringify(parameter.awareness_level.value))
 
     const promises = await Promise.all([
 
