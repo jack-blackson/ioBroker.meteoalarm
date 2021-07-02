76e20fd2dbc1531a00df158aee8c7b1dcf7921a2
Alex <jack-blackson@users.noreply.github.com>
2021-07-02 08:15:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ async function getData(){
                         }
 
 
-                        if ((element['cap:geocode'] == regionCSV) && (expiresDate >= now)){
+                        if ((element['cap:geocode'].value == regionCSV) && (expiresDate >= now)){
                             var detailsLink = element.link[0].$.href
                             adapter.log.debug('4.1: Warning found: ' + detailsLink)
                             detailsURL.push(detailsLink)
