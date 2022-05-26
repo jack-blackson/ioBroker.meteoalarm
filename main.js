f0d1a9490792fd98ca9ffe1d6f384280353bd619
Alex <jack-blackson@users.noreply.github.com>
2022-05-26 22:14:03 +0200

    Update main.js

--- main.js
+++ main.js
@@ -238,7 +238,7 @@ async function getData(){
                 const sentryInstance = adapter.getPluginInstance('sentry');
                 
                 if (sentryInstance) {
-                    adapter.log.debug('Sentry aktiv - Breadcrumb gesetzt: '+ adapter.plugins.sentry.enabled )
+                    adapter.log.debug('Sentry aktiv - Breadcrumb gesetzt: '+ adapter.ioPack.common.plugins.sentry)
                     const Sentry = sentryInstance.getSentryObject();
                     Sentry.addBreadcrumb({
                         category: "info",
